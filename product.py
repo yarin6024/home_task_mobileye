4313ddc550fafc38b082167d7fa779cdb0cf78e2
yarin <yarin6024@gamil.com>
2024-11-02 17:52:58 +0200

    add quantity as a characteristic, and change price type to float

--- product.py
+++ product.py
@@ -1,13 +1,20 @@
 class Product:
-    def __init__(self, name: str, price: int):
+    def __init__(self, name: str, price: float, quantity:int):
         self.name: str = name
-        self.price: int = price
+        self.price: float = price
+        self.quantity = quantity
 
     def get_name(self) -> str:
         return self.name
 
-    def get_price(self) -> int:
+    def get_price(self) -> float:
         return self.price
 
-    def change_price(self, new_price: int):
+    def get_quantity(self) -> int:
+        return self.quantity
+
+    def change_quantity(self, new_quantity: int):
+        self.quantity = new_quantity
+
+    def change_price(self, new_price: float):
         self.price = new_price
